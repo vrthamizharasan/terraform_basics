20c55208f69b18e8c7f8485073647e0a5eb00619
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-03 08:27:05 +0530

    Create variables.tf

---
+++ ec2_withjenkins/variables.tf
@@ -0,0 +1,9 @@
+variable "instancetype" {
+  description = "Using the Instance type"
+  default = "t2.micro"
+}
+
+variable "azone" {
+  description = "Using the AZone "
+  default = "us-east-1a"
+}
