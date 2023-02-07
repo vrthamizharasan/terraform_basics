9ce8733bff8692419525c709b618f6b9bdd93a06
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-07 17:21:31 +0530

    Create locals.tf

---
+++ vpc/locals.tf
@@ -0,0 +1,39 @@
+locals {
+  vpc_cidr = "10.0.0.0/16"
+}
+
+locals {
+  security_group = {
+    public = {
+        name = "public-sg"
+        description = "sg-for the public access"
+        ingress = {
+            ssh = {
+                from = 22
+                to = 22
+                protocol = "tcp"
+                cidr_blocks = [var.accessip]
+            }
+            http = {
+                from = 80
+                to = 80
+                protocol = "tcp"
+                cidr_blocks = [var.accessip]
+            }
+        }
+    }
+    rds = {
+        name = "rds-sg"
+        description = "sg-for the rds access"
+        ingress = {
+            mysql = {
+               from = 3306
+               to = 3306
+               protocol = "tcp"
+               cidr_blocks = local.vpc_cidr
+            }
+        }
+    }
+
+  }
+}
