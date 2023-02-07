00151104dc1e044130f32d0c2dfe1ded5531d663
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-07 13:20:45 +0530

    Create variable.tf

---
+++ vpc/networking/variable.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidrange" {
+}
+
+variable "public_cidrs" {
+}
+
+variable "private_cidrs" {
+}
+
+variable "pubnet_sn_count" {
+}
+
+variable "prinet_sn_count" {
+}
+
+variable "max_subnet" {
+}
