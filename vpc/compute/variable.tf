02f98c1fe2372410f6c547e3bb099fd420cb9976
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-08 21:51:27 +0530

    Create variable.tf

---
+++ vpc/compute/variable.tf
@@ -0,0 +1,25 @@
+variable "demo_instance_type" { }
+
+variable "instance_count" { }
+
+variable "public_sg" { }
+
+variable "public_subnet" { }
+
+variable "vol_size" { }
+
+variable "key_name" { }
+
+variable "pubkey_path" { }
+
+variable "user_data_path" { }
+
+variable "db_endpoint" { }
+
+variable "dbusername" { }
+
+variable "dbpassword" { }
+
+variable "db_name" { }
+
+variable "target_group_arn" { }
