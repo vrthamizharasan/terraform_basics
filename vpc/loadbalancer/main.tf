4564c3818cecca85419471ef3366fbf842251f89
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-08 09:03:46 +0530

    Update main.tf

--- vpc/loadbalancer/main.tf
+++ vpc/loadbalancer/main.tf
@@ -6,3 +6,16 @@ resource "aws_lb" "public_lb" {
     security_groups = [var.public_sg]
     idle_timeout = 400  
 }
+
+resource "aws_alb_target_group" "target_group" {
+  name = "my-tg-${substr(uuid(),0 ,3)}"
+  port = var.tg_port #80
+  protocol = var.tg_protocol #"HTTP"
+  vpc_id = var.vpc_id
+  health_check {
+    healthy_threshold = var.tg_heathythold #2
+    unhealthy_threshold = var.tg_unhealthythold #2
+    interval = var.tg_interval #2
+    timeout = var.tg_timeout #30
+  }
+}
