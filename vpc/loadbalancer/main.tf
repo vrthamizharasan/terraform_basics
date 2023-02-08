3654c581756560e874b4907b21ae70c56f5a952c
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-08 11:07:31 +0530

    Update main.tf

--- vpc/loadbalancer/main.tf
+++ vpc/loadbalancer/main.tf
@@ -12,10 +12,25 @@ resource "aws_alb_target_group" "target_group" {
   port = var.tg_port #80
   protocol = var.tg_protocol #"HTTP"
   vpc_id = var.vpc_id
+  lifecycle {
+    ignore_changes = [name]  //change in the name 
+    create_before_destroy = true  // to support the listerner 
+  }
   health_check {
     healthy_threshold = var.tg_heathythold #2
     unhealthy_threshold = var.tg_unhealthythold #2
     interval = var.tg_interval #2
     timeout = var.tg_timeout #30
   }
+}
+
+
+resource "aws_lb_listener" "lb_listerner" {
+    port = var.lister_port #80
+    protocol = var.lister_protocol #"HTTP"
+    load_balancer_arn = aws_lb.public_lb.arn
+    default_action {
+      type = "forward"
+      target_group_arn = aws_alb_target_group.target_group.arn
+    }
 }
