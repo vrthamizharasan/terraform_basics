1533c65096741ea845f0bf3550ef27925ba4aa03
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-08 09:04:13 +0530

    Update variable.tf

--- vpc/loadbalancer/variable.tf
+++ vpc/loadbalancer/variable.tf
@@ -1,3 +1,9 @@
-variable "publicsubnet" { }
-  
+variable "tg_port" { }
+variable "tg_protocol"   {}
+variable "tg_heathythold" { } 
+variable "tg_unhealthythold" { }
+variable "tg_interval" { }
+variable "tg_timeout" {}
+variable "vpc_id" { }
+variable "publicsubnet" { } 
 variable "public_sg" { }
