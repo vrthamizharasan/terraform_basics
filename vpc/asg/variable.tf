a46593c1936a5ec7765bdf6e27bab70a75f44edc
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-02-23 12:03:50 +0530

    Update variable.tf

--- vpc/asg/variable.tf
+++ vpc/asg/variable.tf
@@ -1,1 +1,18 @@
+variable "asg_name" { }
+
+variable "desired_capacity" { }
+
+variable "max_size" { }
+
+variable "min_size" { }
+
+variable "health_check_grace_period" { } 
+  
+variable "health_check_type" { }
+
+variable "instance_type" { } 
+
+variable "file_path" { }  
+
+variable "public_sg" { }
 
