b08f28538ef6a47ab615b01f5b8abf60f43d1f3e
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-06-18 17:11:48 +0530

    Update provider.tf
    Add the remote back as s3

--- ec2_withjenkins/provider.tf
+++ ec2_withjenkins/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.53.0"
     }
   }
+  backend "s3" {
+    bucket = "terraformstatevr"
+    key = "dev/terraform.statefile"
+    region = "us-east-1"
+    dynamodb_table = "terraform-dev-statefiletable"
+
 }
 
 # provdier block 
