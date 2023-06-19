f6d9873d5fae99629529e94436576d81d546816c
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-06-19 23:31:29 +0530

    Create level3.tf

---
+++ phases/level3.tf
@@ -0,0 +1,70 @@
+//--networkin/main.tf -----
+
+resource "random_integer" "random" {
+  min = 1
+  max = 100
+}
+
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true 
+  enable_dns_support = true 
+  tags = {
+    Name = "mtc_vpc-${random_integer.random.id}"
+  }
+}
+
+resource "aws_subnet" "mtc_public_subnet" {
+  count = var.public_sn_count
+  vpc_id = aws_vpc.mtc_vpc.id 
+  cidr_block = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true 
+  availability_zone = ["us-west-2a", "us-west-2b", "us-west-2c","us-west-2d"][count.index]
+  tags = {
+    Name = "mtc-public-${count.index + 1 }"
+  }
+}
+
+resource "aws_subnet" "mtc_private_subnet" {
+  count = var.private_sn_count
+  vpc_id = aws_vpc.mtc_vpc.id 
+  cidr_block = var.private_cidrs[count.index]
+  map_public_ip_on_launch = false 
+  availability_zone = ["us-west-2a", "us-west-2b", "us-west-2c","us-west-2d"][count.index]
+  tags = {
+    Name = "mtc-private-${count.index + 1 }"
+  }
+}
+
+
+variable "vpc_cidr" {}
+
+variable "public_cidrs" {
+  type = list(string)
+}
+
+variable "private_cidrs" {
+  type = list(string)
+}
+
+variable "public_sn_count" { }
+variable "private_sn_count" { }
+
+output vpc_id  {
+  value = aws_vpc.mtc_vpc.id
+}
+
+
+// [for i in range(2,8,2) : cidrsubnet("10.0.0.0/16" , 8 , i)]
+
+module "networking" {
+  source = "./networking"
+  vpc_cidr = "10.0.0.0/16"
+  public_sn_count = 2
+  private_sn_count = 3
+  public_cidrs =   [for i in range(2,255,2) : cidrsubnet("10.0.0.0/16" , 8 , i)]
+  private_cidrs = [for i in range(1,255,2) : cidrsubnet("10.0.0.0/16" , 8 , i)]
+}
+
+
