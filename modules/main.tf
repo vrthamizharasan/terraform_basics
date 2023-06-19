83c8dc49ac7bc7d02c4a34455835f722a7e804de
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-06-19 22:00:17 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -8,3 +8,9 @@ module "jenkins_iam" {
   iam_policy_name_in       = var.iam_policy_name
   role_name_in             = var.role_name
 }
+
+module "efs_module" {
+  source = "./efs"
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+}
