0a866689e5c4c13e50591657584b95274772515f
Thamizharasan <121797558+vrthamizharasan@users.noreply.github.com>
2023-06-19 22:00:34 +0530

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -12,3 +12,14 @@ variable "role_name" {
   type    = string
   default = "example-role"
 }
+
+variablee "vpc_id" {
+  type = string
+  default = "vpc-00507d09184a16a8d"
+
+}
+
+variable "subnet_ids" {
+  type = list(string)
+  default = ["subnet-044e085d0a093b23b" , "subnet-088da092dca0a3a40","subnet-01bd559b6e269130c","subnet-09dfbd49b87c17058"]
+}
